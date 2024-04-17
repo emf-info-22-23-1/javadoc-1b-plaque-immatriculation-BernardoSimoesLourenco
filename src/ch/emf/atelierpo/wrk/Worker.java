9931b5573b260d876cd5fc281ae71d862f6ff8f3
SimoesB02 <SimoesB02@WSTEMFA44-16.stu.net.fr.ch>
2024-04-17 09:01:58 +0200

    Exercice

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -6,7 +6,23 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-   
+    /**
+     * Description : Cette méthode vérifie si une plaque d'immatriculation est
+     * valide en Suisse. Voici les cantons de Suisse valides : Zurich (ZH), Berne
+     * (BE), Lucerne (LU), Uri (UR), Schwyz (SZ), Obwald (OW), Nidwald (NW), Glaris
+     * (GL), Zoug (ZG), Fribourg (FR), Soleure (SO), Bâle-Ville (BS), Bâle-Campagne
+     * (BL), Schaffhouse (SH), Appenzell Rhodes-Extérieures (AR), Appenzell
+     * Rhodes-Intérieures (AI,) Saint-Gall (SG), Grisons (GR) ,Argovie (AG),
+     * Thurgovie (TG), Tessin (TI), Vaud (VD), Valais (VS) ,Neuchâtel (NE), Genève
+     * (GE) ,Jura (JU)
+     *
+     * @param canton : Identifie le canton où la plaque d'immatriculation est
+     * enregistrée.
+     * @param numeroDePlaque : Le numéro de la plaque d'immatriculation à
+     * contrôler.
+     * @return true si la plaque d'immatriculation est valide en Suisse, sinon
+     * false.
+     */
     public boolean controlePlaqueImmatriculation(String canton, String numeroDePlaque) {
         return false;
     }
